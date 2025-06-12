b6f8e003849eb2aefea6ceff37d946e25368fb53
Nicholas Gimbel <56981761+ngimb64@users.noreply.github.com>
2025-06-11 23:35:00 -0400

    System testing TCP port related to server TLS dial for transfer fixed

--- cmd/kloud-kraken/main.go
+++ cmd/kloud-kraken/main.go
@@ -102,8 +102,8 @@ func handleTransfer(connection net.Conn, buffer []byte, waitGroup *sync.WaitGrou
         return
     }
 
-    // Format remote address with IP and port
-    remoteAddr := ipAddr + ":" + strconv.Itoa(int(port))
+    // Format remote address with IP and  received port for transfer
+    remoteAddr := strings.Split(ipAddr, ":")[0] + ":" + strconv.Itoa(int(port))
 
     // Make a connection to the remote brain server
     transferConn, err := tls.Dial("tcp", remoteAddr,
